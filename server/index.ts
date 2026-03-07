39aa14e009c38cad8a66ca92ba729ac40c215851
m-taqii <busy.digital.master@gmail.com>
2026-03-07 17:27:00 +0500

    feat: initialize server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
